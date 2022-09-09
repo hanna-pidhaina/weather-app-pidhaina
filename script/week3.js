24e0bb0dcb7301cd68d6e38e3eed545e28074f6c
hanna-pidhaina <109957147+hanna-pidhaina@users.noreply.github.com>
2022-09-09 15:49:33 +0300

    Update on weather icon JS

--- script/week3.js
+++ script/week3.js
@@ -1,9 +1,29 @@
-
 let currentTime = new Date();
 
 let time = document.querySelector("#current-time");
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let day = days[currentTime.getDay()];
 let month = months[currentTime.getMonth()];
 let date = currentTime.getDate();
@@ -17,7 +37,7 @@ if (minute < 10) {
 }
 time.innerHTML = `${day}, ${month} ${date}, ${hour}:${minute}`;
 
-function search (city){
+function search(city) {
   let units = "metric";
   let apiKey = "a3a670287c6f4b3ee8710439a67cc382";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=${units}&q=${city}`;
@@ -33,114 +53,83 @@ function showCity(event) {
 let searchCity = document.querySelector("#search-city");
 searchCity.addEventListener("submit", showCity);
 
-///let celsiusTemperature = null;
-
-
-function showWeather (response){
+function showWeather(response) {
   console.log(response.data);
 
   let cityName = document.querySelector("h3");
   cityName.innerHTML = response.data.name;
-  
+
   let temperature = document.querySelector("#temp");
   celsiusTemperature = Math.round(response.data.main.temp);
   temperature.innerHTML = celsiusTemperature;
-  
+
   let precipitation = document.querySelector("#precipitation");
   precipitation.innerHTML = response.data.weather[0].main;
-  
+
   let wind = document.querySelector("#wind");
   wind.innerHTML = `Wind ${response.data.wind.speed} m/s`;
-  
+
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity ${response.data.main.humidity} %`;
-  console.log (response.data.weather[0].id);
-  console.log (Math.round(Date.now()/1000));
+  console.log(response.data.weather[0].id);
+  console.log(Math.round(Date.now() / 1000));
   console.log(response.data.sys.sunset);
 
   let mainIcon = document.querySelector("#main-icon");
-  let weatherDescription = response.data.weather[0].main;
-  let weatherId = response.data.weather[0].id;
+  let weatherIcon = response.data.weather[0].icon;
   let sunrise = response.data.sys.sunrise;
   let sunset = response.data.sys.sunset;
-  let unixTime = Math.round(Date.now()/1000);
-  console.log(mainIcon.innerHTML);
+  let unixTime = Math.round(Date.now() / 1000);
 
-   if (weatherDescription === "Thunderstorm" && sunrise < unixTime < sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-bolt"></i>`;
-   }
-   else if (weatherDescription === "Thunderstorm" && unixTime > sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-bolt-moon"></i>`;
-   }
-   else if (weatherDescription === "Drizzle") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-drizzle"></i>`;
-   }
-   else if ([500, 501].includes(weatherId) && sunrise < unixTime < sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-sun-rain"></i>`;
-   }
-   else if ([500, 501].includes(weatherId) && unixTime > sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-moon-rain"></i>`;
-   }
-   else if ([502, 503, 504, 511].includes(weatherId)) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
-   }
-   else if ([520, 521].includes(weatherId)) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-showers"></i>`;
-   }
-   else if ([522, 531].includes(weatherId)) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-showers-heavy"></i>`;
-   }
-   else if (weatherDescription === "Snow") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
-   }
-
-   else if (["Mist", "Fog"].includes(weatherDescription)) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-fog"></i>`;
-   }
-   else if (weatherDescription === "Smoke") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-smoke"></i>`;
-   }
-   else if (weatherDescription === "Haze") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-sun-haze"></i>`;
-   }
-   else if (["Dust", "Sand", "Ash"].includes(weatherDescription)) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-sun-dust"></i>`;
-   }
-   else if (weatherDescription === "Squall") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-wind-warning"></i>`;
-   }
-   else if (weatherDescription === "Tornado") {
-     mainIcon.innerHTML = `<i class="fa-sharp fa-solid fa-tornado"></i>`;
-   }
-   else if (weatherDescription === "Clear" && sunrise < unixTime < sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-sun-cloud"></i>`;
-   }
-   else if (weatherDescription === "Clear" && unixTime > sunset) {
+   //let {icon} = conds.find((x) => x.cond);
+//mainIcon.innerHTML = `<i class="fa-solid ${icon}"></i>`;
+  if (weatherIcon === "01d") {
+    mainIcon.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+  }
+  else if (weatherIcon === "01n") {
      mainIcon.innerHTML = `<i class="fa-solid fa-moon"></i>`;
-   }
-   else if (weatherId === "801" && sunrise < unixTime < sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-clouds-sun"></i>`;
-   }
-   else if (weatherId === "801" && unixTime > sunset) {
-     mainIcon.innerHTML = `<i class="fa-solid fa-clouds-moon"></i>`;
-   }
-   else if (weatherId === "803") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
-   }
-   else if (weatherId === "804") {
-     mainIcon.innerHTML = `<i class="fa-solid fa-clouds"></i>`;
-   }
+  }
+  else if (weatherIcon === "02d") {
+     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-sun"></i>`;
+  }
+  else if (weatherIcon === "02n") {
+     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-moon"></i>`;
+  }
+  else if (["03d", "03n"].includes(weatherIcon)) {
+    mainIcon.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+  }
+  else if (["04d", "04n"].includes(weatherIcon)) {
+    mainIcon.innerHTML = `<i class="fa-solid fa-cloud"></i>`;
+  }
+  else if (["09d", "09n"].includes(weatherIcon)) {
+    mainIcon.innerHTML = `<i class="fa-solid fa-cloud-rain"></i>`;
+  }
+  else if (weatherIcon === "10d") {
+     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-sun-rain"></i>`;
+  }
+  else if (weatherIcon === "10n") {
+     mainIcon.innerHTML = `<i class="fa-solid fa-cloud-moon-rain"></i>`;
+  }
+  else if (["11d", "11n"].includes(weatherIcon)) {
+    mainIcon.innerHTML = `<i class="fa-solid fa-cloud-bolt"></i>`;
+  }
+  else if (["13d", "13n"].includes(weatherIcon)) {
+    mainIcon.innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
+  }
+   else {
+    mainIcon.innerHTML = `<i class="fa-solid fa-smog"></i>`;
+  }
+
 }
 
-function getPosition (position){
+function getPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "a3a670287c6f4b3ee8710439a67cc382";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${latitude}&lon=${longitude}&units=metric`;
   axios.get(apiUrl).then(showWeather);
-  
 }
-function showCurrentLocation (){
+function showCurrentLocation() {
   navigator.geolocation.getCurrentPosition(getPosition);
 }
 let currentLocation = document.querySelector(".current-location-button");
@@ -165,8 +154,4 @@ function changeToF(event) {
 }
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", changeToF);
-
-search ("Kyiv");
-
-
-
+search("Kyiv");
